64e673619810a47444eab099d2ae0e37c58cb231
Rahul Gangwar <105579080+maestroNeal@users.noreply.github.com>
2023-06-27 20:22:34 +0530

    First commit

---
+++ bank/management/system/Conn.java
@@ -0,0 +1,24 @@
+
+package bank.management.system;
+
+import java.sql.*;
+
+
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///bms", "root","Rahul@123");
+            s=c.createStatement();
+            
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+}
